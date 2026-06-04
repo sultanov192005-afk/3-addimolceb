a136df4db7023b9a00eb680e524fcf4603b14e55
sultanov192005-afk <sultanov192005@gmail.com>
2026-06-04 20:19:08 +0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+let addim = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(addim)
+})
+input.onButtonPressed(Button.B, function () {
+    addim = 0
+})
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.Strength) > 1500) {
+        addim += 1
+    }
 })
